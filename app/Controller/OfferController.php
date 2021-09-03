64b6d2699c08d7a93e5fbc35f393def4857a20ca
zearkiatos <caprilespe@outlook.com>
2021-09-02 23:48:49 -0400

    The emails sistem

--- app/Controller/OfferController.php
+++ app/Controller/OfferController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 use App\Entity\Offer;
 use App\Entity\Applicant;
 
@@ -32,7 +33,7 @@ class OfferController extends AbstractController
      * @Route("/offer/{id}/apply", name="offer_apply")
      * @IsGranted("ROLE_APPLICANT")
      */
-    public function apply(Offer $offer, EntityManagerInterface $entityManager)
+    public function apply(Offer $offer, EntityManagerInterface $entityManager, MailerInterface $mailer)
     {
         if ($applicant = $entityManager->getRepository(Applicant::class)->findOneBy([
             'user' => $user,
@@ -42,6 +43,13 @@ class OfferController extends AbstractController
             try {
                 $entityManager->flush();
                 $this->addFlash('success', 'Request recived');
+                $email = (new Email())
+                    ->from('caprilespe@gmail.com')
+                    ->to($offer->getOwner()->getEmail())
+                    ->subject('It was recived a new application for '.$offer->getName().'!')
+                    ->html('<p>'.$applicant->getName().' is intereste. His/her email is '.$applicant->getEmail().'</p>');
+                    $mailer->send($email);
+
             } catch (\Exception $exception) {
                 $this->addFlash('danger', 'The request couldn\'t be storage. Please, try again.');
             }
